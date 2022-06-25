bf34e9303aab2b7b9f2f74fd707a4f1e66223543
Mark Winters <nulimitz360@gmail.com>
2022-06-25 11:03:23 -0600

    Created login and register pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,40 +5,42 @@ function Login() {
     <div className="container">
       <div class="row mt-5">
         <div class="col-md-6 m-auto">
-          <div class="card card-body">
-            <h1 class="text-center mb-3">Login</h1>
-            <form action="/users/login" method="POST">
-              <div class="mb-3">
-                <label for="email" className="form-label">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  class="form-control"
-                  placeholder="Enter your email"
-                />
-              </div>
-              <div class="mb-3">
-                <label for="password" className="form-label">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  class="form-control"
-                  placeholder="Enter password"
-                />
-              </div>
-              <button type="submit" class="btn btn-primary">
-                Login
-              </button>
-            </form>
-            <p class="mt-4">
-              No Account? <a href="/register">Register</a>
-            </p>
+          <div class="card">
+            <div className="card-body">
+              <h1 class="text-center mb-3">Login</h1>
+              <form action="/users/login" method="POST">
+                <div class="mb-3">
+                  <label for="email" className="form-label">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    class="form-control"
+                    placeholder="Enter your email"
+                  />
+                </div>
+                <div class="mb-3">
+                  <label for="password" className="form-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    class="form-control"
+                    placeholder="Enter password"
+                  />
+                </div>
+                <button type="submit" class="btn btn-primary">
+                  Login
+                </button>
+              </form>
+              <p class="mt-4">
+                No Account? <a href="/register">Register</a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,64 +5,66 @@ function Register() {
     <div className="container">
       <div className="row mt-5">
         <div className="col-md-6 m-auto">
-          <div className="card card-body">
-            <h1 className="text-center mb-3">Register</h1>
-            <form action="/users/login" method="POST">
-              <div className="mb-3">
-                <label for="name" classNameName="form-label">
-                  Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  className="form-control"
-                  placeholder="Enter your name"
-                />
-              </div>
-              <div className="mb-3">
-                <label for="email" classNameName="form-label">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  className="form-control"
-                  placeholder="Enter your Email"
-                />
-              </div>
-              <div className="mb-3">
-                <label for="password" classNameName="form-label">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  className="form-control"
-                  placeholder="Enter password"
-                />
-              </div>
-              <div className="mb-3">
-                <label for="password2" classNameName="form-label">
-                  Confirm Password
-                </label>
-                <input
-                  type="password"
-                  id="password2"
-                  name="password2"
-                  className="form-control"
-                  placeholder="Confirm Password"
-                />
-              </div>
-              <button type="submit" className="btn btn-primary">
-                Register
-              </button>
-            </form>
-            <p className="mt-4">
-              Already have an account? <a href="/login">Login</a>
-            </p>
+          <div className="card">
+            <div className="card-body">
+              <h1 className="text-center mb-3">Register</h1>
+              <form action="/users/login" method="POST">
+                <div className="mb-3">
+                  <label for="name" classNameName="form-label">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    className="form-control"
+                    placeholder="Enter your name"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label for="email" classNameName="form-label">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="form-control"
+                    placeholder="Enter your Email"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label for="password" classNameName="form-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    className="form-control"
+                    placeholder="Enter password"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label for="password2" classNameName="form-label">
+                    Confirm Password
+                  </label>
+                  <input
+                    type="password"
+                    id="password2"
+                    name="password2"
+                    className="form-control"
+                    placeholder="Confirm Password"
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Register
+                </button>
+              </form>
+              <p className="mt-4">
+                Already have an account? <a href="/login">Login</a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
