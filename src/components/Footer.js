9b9d3d4ca2ed3b0899aaf04c258dde07314d8f0e
Mark Winters <nulimitz360@gmail.com>
2022-06-25 08:28:09 -0600

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className="container text-center">
+            <p>Copyright &copy; 2022</p>
+        </div>
+    </footer>
+  );
+};
+
+export default Footer;
