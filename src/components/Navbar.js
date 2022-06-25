e920567dac932f9008bd89be5bd0ff3ca260b6db
Mark Winters <nulimitz360@gmail.com>
2022-06-25 10:58:54 -0600

    Created login and register pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <a className="navbar-brand" href="/">
-          Navbar
+          React App
         </a>
         <button
           className="navbar-toggler"
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="container">
+      <div class="row mt-5">
+        <div class="col-md-6 m-auto">
+          <div class="card card-body">
+            <h1 class="text-center mb-3">Login</h1>
+            <form action="/users/login" method="POST">
+              <div class="mb-3">
+                <label for="email" className="form-label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  class="form-control"
+                  placeholder="Enter your email"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  class="form-control"
+                  placeholder="Enter password"
+                />
+              </div>
+              <button type="submit" class="btn btn-primary">
+                Login
+              </button>
+            </form>
+            <p class="mt-4">
+              No Account? <a href="/register">Register</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
